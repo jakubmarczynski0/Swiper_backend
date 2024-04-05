f0454e2086e5ab37aab7486c27ea27ae8dd00b98
philljohnson <philljohnson739@gmail.com>
2024-04-05 12:12:22 -0300

    fix: add transaction

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
 const express = require("express");
-const { getCosmWasmClient } = require("@sei-js/core");
+const { getCosmWasmClient, getSigningCosmWasmClient } = require("@sei-js/core");
+const { calculateFee, GasPrice } = require("@cosmjs/stargate");
+const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const cosmWasmClient = getCosmWasmClient("https://sei-rpc.brocha.in/");
+const rpcEndpoint = "https://sei-rpc.brocha.in/";
+const cosmWasmClient = getCosmWasmClient(rpcEndpoint);
 let roundInfo = {};
 
 app.use(cors());
+
+const handleTransation = async () => {
+  const gasPrice = GasPrice.fromString("0.1usei");
+  const execute_fee = calculateFee(200000, gasPrice);
+  const sender_wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+    process.env.MNEMONIC,
+    { prefix: "sei" }
+  );
+  const sender_client = await getSigningCosmWasmClient(
+    rpcEndpoint,
+    sender_wallet
+  );
+
+  let execute_msg = {
+    decide_winner: {},
+  };
+
+  try {
+    let tx = await sender_client.execute(
+      process.env.NEXT_PUBLIC_ADMIN_ADDRESS,
+      process.env.NEXT_PUBLIC_SWIPER_ADDRESS,
+      execute_msg,
+      execute_fee
+    );
+    console.log("tx", tx);
+  } catch (e) {
+    console.log("error", e);
+  }
+};
 
 const fetchRoundInfo = async () => {
   const response = await (
@@ -22,6 +53,14 @@ const fetchRoundInfo = async () => {
 const fetchUpdate = async () => {
   try {
     roundInfo = await fetchRoundInfo();
+    if (
+      roundInfo?.config.round_time +
+        roundInfo?.round_info?.round_start_time -
+        roundInfo?.current_time <=
+      0
+    ) {
+      handleTransation();
+    }
   } catch (e) {
     console.log(e);
   }
